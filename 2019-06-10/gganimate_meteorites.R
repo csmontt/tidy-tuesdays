7ac22adb50e5b5ee83f4599851e28b45e6664353
csmontt <c.s.montt@gmail.com>
2019-06-10 21:57:07 +0100

    add comments to script

--- 2019-06-10/gganimate_meteorites.R
+++ 2019-06-10/gganimate_meteorites.R
@@ -11,19 +11,24 @@ library(ggplot2)
 library(ggthemes)
 library(gganimate)
 
+
+# Get base map
 world <- ggplot() +
   borders("world", colour = "#353535", fill = "#353535") +
   theme_map()
 
+# Read data
 meteorites <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-11/meteorites.csv")
 
 meteorites <- meteorites[!is.na(meteorites$lat) & !is.na(meteorites$year), ]
 
+# Took the log of mass because and divided it by 5 so when using this new 
+# variable to size the geom_points in ggplot, they weren't too big.
 meteorites <- meteorites %>% mutate(log_mass = log(mass)/5) %>%
         filter(year >= 1800 & year <= 20013)
 
 
-
+# Specifiy vis
 met_map <- world + geom_point(data = meteorites, 
                x = meteorites$long,
                y = meteorites$lat,
@@ -44,5 +49,13 @@ met_map <- world + geom_point(data = meteorites,
 options(gganimate.dev_args = list(width = 1000, height = 600))
 ani_met <- animate(met_map, nframes = 300, fps=10, detail = 1)
 
+# Save GIF
 anim_save("./figures/2019-06-10.gif")
+
+# To do:
+# Should have specified more frames as 300 were not enough to show all 
+# years with data.
+# Add a lbel for the mass of the meteorites, maybe create categories instead
+# of using a continous variable.
+# add mapping by color to denote type of meteorite.
 
