7f2e6c9419b75629ab62b6d9f638352b6b92b08d
csmontt <c.s.montt@gmail.com>
2019-07-02 12:43:18 +0100

    media franchise plot submission

---
+++ 2019-07-02/box_plot_media_franchise.R
@@ -0,0 +1,47 @@
+########################################################################
+## Project: tidytuesdays
+## Script purpose: a simple annotated boxplot
+##
+##
+## Date: 02-07-2019
+## Author: csmontt
+########################################################################
+library(devtools)
+library(ggplot2)
+library(extrafont)
+library(here)
+loadfonts(device = "win")
+
+source(here("2019-07-02", "custom_theme.R"))
+
+# load data --------------------------------------------------------------------------                     
+media_franchises <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-02/media_franchises.csv")
+
+# delete duplicates
+media_franchises <- media_franchises[!duplicated(media_franchises), ]
+
+# get most succesful media franchise by cateory
+max_revenue <-  media_franchises %>% group_by(revenue_category) %>% 
+        slice(which.max(revenue)) %>% tidyr::unnest()
+
+# create plot
+box_media <- ggplot(media_franchises, 
+              aes(x = factor(revenue_category), y = revenue, fill = revenue_category)) + 
+        geom_boxplot() + custom_theme() +
+        geom_text(data = max_revenue, 
+              aes(x = factor(revenue_category), y = revenue, label = franchise, 
+                  family = "Century Gothic"), 
+              nudge_x = 0, nudge_y = 2, size = 2) +
+        labs(title="Most succesful media franchises",
+             subtitle = "Revenue in billions by media category",
+             caption = "Source: Wikipedia | Vis: @csmontt") +
+        guides(fill=guide_legend(nrow=1))
+
+# save plot
+ ggsave(here("figures", "media_franchise_boxplot.png"), width = 13, height = 8)
+
+
+
+
+
+                
---
+++ 2019-07-02/custom_theme.R
@@ -0,0 +1,30 @@
+custom_theme <- function () 
+{
+    font <- "Century Gothic"
+    ggplot2::theme(plot.title = ggplot2::element_text(family = font, 
+                   size = 18, face = "bold", color = "#222222"), 
+                   plot.subtitle = ggplot2::element_text(family = font, 
+                   size = 12, 
+                   margin = ggplot2::margin(1, 0, 10, 0)), 
+                   legend.position = "top", 
+                   legend.text.align = 0, 
+                   legend.title = ggplot2::element_blank(), 
+                   legend.key = ggplot2::element_blank(), 
+                   legend.text = ggplot2::element_text(family = font, size = 10, 
+                   color = "#222222"), 
+                   legend.direction = "horizontal",
+                   axis.title = ggplot2::element_blank(), 
+                   axis.text = ggplot2::element_text(family = font, size = 10, 
+                   color = "#222222"), 
+                   axis.text.x = element_blank(), 
+                   axis.ticks = ggplot2::element_blank(), 
+                   axis.line = ggplot2::element_blank(), 
+                   panel.grid.minor = ggplot2::element_blank(), 
+                   #horizontal line color
+        panel.grid.major.y = ggplot2::element_line(color = "#cbcbcb"), 
+        panel.grid.major.x = ggplot2::element_blank(), 
+        plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+        panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+        legend.background = element_rect(fill = "#f5f5f2", color = NA),
+        strip.text = ggplot2::element_text(size = 12, hjust = 0))
+}
